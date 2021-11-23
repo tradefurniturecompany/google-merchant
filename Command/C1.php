f372100d93895a332c4f66e024829154f84fe48b
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-24 02:24:46 +0600

    https://github.com/tradefurniturecompany/google-shopping/issues/1

--- Command/C1.php
+++ Command/C1.php
@@ -2,6 +2,8 @@
 namespace TFC\GoogleShopping\Command;
 use Google\Client as C;
 use Google\Service\ShoppingContent as S;
+use Google\Service\ShoppingContent\Account as Acc;
+use Google\Service\ShoppingContent\AccountIdentifier as AccId;
 use Google\Service\ShoppingContent\AccountsAuthInfoResponse as AuthInfo;
 # 2021-11-22
 # "Setup an automatic integration between Magento and Google Merchant Center":
@@ -37,8 +39,26 @@ final class C1 extends \Df\Framework\Console\Command {
 		$c->setScopes(S::CONTENT);
 		# 2021-11-24 https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L118
 		$s = new S($c); /** @var S $s */
-		# 2021-11-24 https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L195
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L195
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/accounts/authinfo
 		$authInfo = $s->accounts->authinfo(); /** @var AuthInfo $authInfo */
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L206
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/accounts/authinfo#body.AccountsAuthInfoResponse.FIELDS.account_identifiers
+		$accId = df_first($authInfo->getAccountIdentifiers()); /** @var AccId $accId */
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L208
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/accounts/authinfo#AccountIdentifier.FIELDS.merchant_id
+		$merchantId = (int)$accId->getMerchantId(); /** @var int $merchantId */
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L233
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/accounts/get
+		$acc = $s->accounts->get($merchantId, $merchantId); /** @var Acc $acc */
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ContentSession.php#L235
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/accounts#Account.FIELDS.website_url
+		$websiteUrl = $acc->getWebsiteUrl(); /** @var string $websiteUrl */
 		$this->output()->writeln(__METHOD__);
 	}
 }
