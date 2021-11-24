2722a890e6be614602d9d662867dced13b4c046d
Dmitry Fedyuk <dfediuk@gmail.com>
2021-11-24 22:15:44 +0600

    https://github.com/tradefurniturecompany/google-shopping/issues/1

--- Command/C1.php
+++ Command/C1.php
@@ -137,6 +137,23 @@ final class C1 extends \Df\Framework\Console\Command {
 		# 3) «Required. Your product’s landing page.» https://support.google.com/merchants/answer/7052112#link
 		# 4) https://support.google.com/merchants/answer/6324416
 		$r->setLink($p->getProductUrl());
+		# 2021-11-24
+		# 1) https://github.com/googleads/googleads-shopping-samples/blob/053bc550/php/ProductsSample.php#L218-L219
+		# 2) https://developers.google.com/shopping-content/reference/rest/v2.1/products#Product.FIELDS.image_link
+		# 3) «Required. The URL of your product’s main image. Don't submit a placeholder or a generic image.»
+		# https://support.google.com/merchants/answer/7052112#image_link
+		# 4) «If you have multiple, different images of the product,
+		# submit the main image using the image link [image_link] attribute,
+		# and include all other images in the additional image link [additional_image_link] attribute.»
+		# https://support.google.com/merchants/answer/6324350
+		# 5) «Once you submit your product data, the image will typically be crawled within 3 days.»
+		# https://support.google.com/merchants/answer/6324350#urlguidelines
+		# 6) «If you need to change an image of an existing product, submit a new URL for the new image.
+		# When you submit a new URL, the image will be crawled within 3 days.
+		# Keep in mind that if you change the image, but keep the same URL,
+		# it may take a while to detect the change and recrawl the new image.»
+		# https://support.google.com/merchants/answer/6324350#urlguidelines
+		$r->setImageLink(df_product_image_url($p));
 		return $r;
 	}
 }
